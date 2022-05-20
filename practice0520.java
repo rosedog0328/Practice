dd4bd5ed58b88da6d14aa0ae61b665d8c5214aa0
beth321 <rosedog0328@gmail.com>
2022-05-20 16:23:30 +0800

    practice0520

---
+++ practice0520.java
@@ -0,0 +1,16 @@
+public class practice0520
+{
+	public static void main(String args[])
+	{
+		int i;
+		for(i=1; i<=9; i++)
+		{
+			int j;
+			for(j=1; j<=9; j++)
+			{
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
+	}
+}
