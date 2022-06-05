187772b4abdb3f97c51abda114607d897e736d27
beth321 <rosedog0328@gmail.com>
2022-06-05 18:30:48 +0800

    practice0605

---
+++ practice0605.java
@@ -0,0 +1,14 @@
+public class practice0605
+{
+	public static void main(String args[])
+	{
+		System.out.println("1*2*...*4="+fac(4));
+	}
+	public static int fac(int n)
+	{
+		if(n==0)
+			return 1;
+		else
+			return n*fac(n-1);
+	}
+}
